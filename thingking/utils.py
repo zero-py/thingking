092211dc77787a82c07cca1a4dcf787661abaf96
Mike Warren <mswarren@gmail.com>
2014-07-08 11:38:08 -0600

    Implement isurl()

--- thingking/utils.py
+++ thingking/utils.py
@@ -2,13 +2,20 @@ from httpmmap import HTTPArray
 from cStringIO import StringIO
 import numpy as np
 
+def isurl(path):
+    if path.startswith('http://'): return True
+    if path.startswith('https://'): return True
+    if path.startswith('ftp://'): return True
+    if path.startswith('file://'): return True
+    return False
+
 def loadtxt(path, **kwargs):
     """
     Load data from a local or remote HTTP file.
 
     numpy.loadtxt docs:
     """
-    if 'http' in path:
+    if isurl(path):
         data = HTTPArray(path)
         return np.loadtxt(StringIO(data[:]), **kwargs)
     return np.loadtxt(path, **kwargs)
