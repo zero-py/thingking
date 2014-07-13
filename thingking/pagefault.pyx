07b28b08e0daa2ea8282d126870fde95d6d71e74
Matthew Turk <matthewturk@gmail.com>
2014-07-12 19:48:22 -0500

    Sigsegv handling approaching working.

--- thingking/pagefault.pyx
+++ thingking/pagefault.pyx
@@ -2,18 +2,27 @@ cimport numpy as np
 import numpy as np
 import mmap
 cdef sigsegv_dispatcher thingking_dispatch
+cdef object my_mmap_obj
 cdef mmap_object my_mmap
 
-cdef int handler(void *fault_address, void *user_arg):
+cdef int handler(void *fault_address, int serious):
+    return sigsegv_dispatch(&thingking_dispatch, fault_address);
+
+cdef int fault_handler(void *fault_address, void *user_arg):
     print "Fault address", <np.int64_t> (fault_address - <void*>my_mmap.data)
+    return 1
 
 def setup(mmap_obj, page_to_protect):
+    cdef int rv
     if not isinstance(mmap_obj, mmap.mmap):
         raise RuntimeError
     sigsegv_init(&thingking_dispatch)
-    cdef sigsegv_area_handler_t h = handler
-    global my_mmap
-    my_mmap = mmap_obj
+    sigsegv_install_handler(&handler)
+    global my_mmap, my_mmap_obj
+    my_mmap_obj = mmap_obj
+    my_mmap = (<mmap_object*> (<PyObject*> mmap_obj))[0]
     sigsegv_register(&thingking_dispatch, my_mmap.data, my_mmap.size,
-                     &handler, &my_mmap)
-    mprotect(<void *>my_mmap.data, my_mmap.size, PROT_READ)
+                     &fault_handler, &my_mmap)
+    print "RV2", rv
+    rv = mprotect(<void *> my_mmap.data, my_mmap.size, PROT_NONE)
+    print "RV3", rv
