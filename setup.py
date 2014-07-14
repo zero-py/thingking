73f51d9dad7f5e738218b930febb4bb61aa2be3e
Matthew Turk <matthewturk@gmail.com>
2014-07-14 16:53:45 -0500

    Get rid of setuptools, make the pagefault.so go in the right place.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup, find_packages
+from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Build.Dependencies import create_extension_list
@@ -8,6 +8,7 @@ README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read() + 'nn'
 source_files = ["thingking/pagefault.pyx"]
 extensions = [Extension("thingking/pagefault", source_files)]
+modules = cythonize(extensions)
 setup(name='thingking',
       version=version,
       description=("A memory map for the World Wide Web"),
@@ -19,7 +20,7 @@ setup(name='thingking',
       keywords='data',
       author='Matthew Turk <matthewturk@gmail.com>, Samuel Skillman <samskillman@gmail.com>, Michael S. Warren <mswarren@gmail.com>',
       license='BSD',
-      packages=find_packages(),
+      packages=["thingking"],
       install_requires=["requests", "numpy", "functools32"],
-      ext_modules = cythonize(extensions),
+      ext_modules = cythonize(modules),
 )
